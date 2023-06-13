32cb407fe8ec1b1d6d77dd64f9d2d5c01bbc73ea
harsha <harshab6345@gmail.com>
2023-06-13 10:26:41 +0530

    I made another sqaure with columns and rows alternatively printing numbers

--- square.py
+++ square.py
@@ -1,5 +1,9 @@
 for x in range(1, 11, 1):
     line = ""
-    for y in range(1, 11, 1):
-        line += str(y)
+    if x % 2 != 0:
+        for y in range(1, 11, 1):
+            line += str(y) + " "
+    else:
+        for y in range(10, 0, -1):
+            line += str(y) + " "
     print(line)
