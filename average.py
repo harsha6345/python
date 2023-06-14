b15140821712d5d7793c754317403beb1260ef2f
harsha <harshab6345@gmail.com>
2023-06-14 10:06:59 +0530

    Added HM and GM

--- average.py
+++ average.py
@@ -6,5 +6,18 @@ for i in range(n):
 sum = 0
 for i in range(n):
     sum += numberarr[i]
+multiply = 1
+for i in range(n):
+    multiply *= numberarr[i]
+
+gm = multiply ** (1 / n)
+denom = 0
+for i in range(n):
+    denom += 1/numberarr[i]
+
+hm = n / denom
+
 avg = sum / n
-print("The average is ", avg)
+print("The Arithmetic mean is ", avg)
+print("Geometric mean is ", gm)
+print("Harmonic mean is ", hm)
